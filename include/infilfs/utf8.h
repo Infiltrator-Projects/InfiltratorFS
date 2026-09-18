a80f61b08f62ae9995ad15a613abeff6787327c0
Shannon Smith <infiltratr@yandex.com>
2026-09-18 20:11:55 +1000

    Alias filesystem UTF-8 validation to Common

--- include/infilfs/utf8.h
+++ include/infilfs/utf8.h
@@ -3,7 +3,11 @@
 #define INFILFS_UTF8_H
 
 #include <stddef.h>
+#include "infiltratr/utf8.h"
 
-int infs_utf8_validate(const void *bytes, size_t length);
+static inline int infs_utf8_validate(const void *bytes, size_t length)
+{
+    return infiltratr_utf8_validate(bytes, length) ? 1 : 0;
+}
 
 #endif
