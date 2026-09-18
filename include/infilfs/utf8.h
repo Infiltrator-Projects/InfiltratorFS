1e49544ae03031448d183ac7bb46b9982ba5a9e0
Shannon Smith <infiltratr@yandex.com>
2026-09-18 20:40:27 +1000

    Alias UTF-8 validation directly to Common

--- include/infilfs/utf8.h
+++ include/infilfs/utf8.h
@@ -2,12 +2,8 @@
 #ifndef INFILFS_UTF8_H
 #define INFILFS_UTF8_H
 
-#include <stddef.h>
 #include "infiltratr/utf8.h"
 
-static inline int infs_utf8_validate(const void *bytes, size_t length)
-{
-    return infiltratr_utf8_validate(bytes, length) ? 1 : 0;
-}
+#define infs_utf8_validate infiltratr_utf8_validate
 
 #endif
