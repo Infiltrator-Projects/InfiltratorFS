35b370946ba72025edd39da6a2eb6f7d0fb26aa9
Shannon Smith <infiltratr@yandex.com>
2026-09-18 20:11:57 +1000

    Remove redundant UTF-8 wrapper source

--- src/utf8.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-#include "infilfs/utf8.h"
-#include "infiltratr/utf8.h"
-
-int infs_utf8_validate(const void *bytes, size_t length)
-{
-    return infiltratr_utf8_validate(bytes, length) ? 1 : 0;
-}
