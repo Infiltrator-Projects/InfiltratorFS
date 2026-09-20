f029c1fb492d7dda44208fb0acb369017207d47b
Shannon Smith <infiltratr@yandex.com>
2026-09-20 17:42:01 +1000

    Expose reserved Linux metadata name predicate

--- kernel/infiltratorfs_internal.h
+++ kernel/infiltratorfs_internal.h
@@ -71,6 +71,7 @@ void infilfs_cpu_work_enter(void);
 void infilfs_cpu_work_exit(void);
 bool infilfs_removable_name_valid_v1(const unsigned char *name, size_t length);
 int infilfs_name_validate(struct super_block *sb, const struct qstr *name);
+bool infilfs_name_is_reserved_linux_meta(const struct qstr *name);
 
 static inline size_t infilfs_native_writeback_batch_bytes(void)
 {
