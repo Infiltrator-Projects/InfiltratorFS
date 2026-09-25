376ef36dda3b2d5f4fad24e12e4440a6e5d23f6e
Shannon Smith <infiltratr@yandex.com>
2026-09-25 11:30:44 +1000

    Expose Windows security roundtrip helpers

--- tools/windows/infiltratorfs-windows-metadata.h
+++ tools/windows/infiltratorfs-windows-metadata.h
@@ -7,6 +7,7 @@
 #include <windows.h>
 
 #include "infilfs/volume.h"
+#include "infilfs/win32_security.h"
 
 int infilfs_windows_ticks_to_timestamp(INT64 ticks,
                                        struct infs_timestamp *out);
@@ -16,6 +17,12 @@ uint64_t infilfs_windows_attributes_to_portable(DWORD attributes);
 DWORD infilfs_portable_to_windows_attributes(uint64_t flags, int directory);
 void infilfs_windows_basic_to_time_update(
     const FILE_BASIC_INFO *basic, struct infs_time_update *update);
+infs_status infilfs_windows_import_security(
+    struct infs_volume *volume, const char *path,
+    const wchar_t *native_path, int directory);
+infs_status infilfs_windows_export_security(
+    struct infs_volume *volume, const char *path,
+    const wchar_t *native_path, int directory);
 #endif
 
 #endif
