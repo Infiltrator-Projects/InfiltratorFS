390e00baf694ff57e2cf3fa1cf771463067e1695
Shannon Smith <infiltratr@yandex.com>
2026-09-26 20:43:31 +1000

    Share atomic namespace replacement helper

--- src/volume.c
+++ src/volume.c
@@ -182,6 +182,8 @@ static infs_status generate_unique_object_id(struct infs_volume *vol,
 static infs_status file_free_unshared_run(
     struct infs_volume *vol, const uint8_t owner_id[16],
     uint64_t start, uint64_t count);
+static int destroy_replaced_object_tx(
+    struct infs_volume *vol, const struct infs_lookup *target);
 static infs_status file_read_small_content(
     struct infs_volume *vol, uint8_t object[INFS_BLOCK_SIZE],
     uint8_t data[INFS_INLINE_DATA_MAX]);
